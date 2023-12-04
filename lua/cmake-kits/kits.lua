0b4939874efc8edf12514531c5edb7d241fd1409
XDeme <fernando.tagawa.gamail.com@gmail.com>
2023-12-04 16:39:43 -0300

    Separate save and load of json

--- lua/cmake-kits/kits.lua
+++ lua/cmake-kits/kits.lua
@@ -1,4 +1,5 @@
 local job = require("plenary.job")
+local utils = require("cmake-kits.utils")
 
 --- @class cmake-kits.Kit
 --- @field name string
@@ -11,34 +12,15 @@ local M = {}
 
 M.kits = {}
 
-M.load_kits = function()
-    local vscode_path = vim.fs.normalize("$HOME")
-        .. "/.local/share/CMakeTools/cmake-tools-kits.json"
+local save_path =
+    vim.fs.joinpath(vim.fs.normalize("$HOME"), ".local/share/CMakeTools/cmake-tools-kits.json")
 
-    local file = io.open(vscode_path, "r")
-    if file then
-        M.kits = vim.json.decode(file:read("*a"))
-        file:close()
-        return
-    end
+M.load_kits = function()
+    M.kits = utils.load_data(save_path)
 end
 
 M.save_kits = function()
-    local vscode_path = vim.fs.normalize("$HOME")
-        .. "/.local/share/CMakeTools/cmake-tools-kits.json"
-
-    local old_file = io.open(vscode_path, "r")
-    local json = {}
-    if old_file then
-        json = vim.json.decode(old_file:read("*a"))
-        old_file:close()
-    end
-    vim.tbl_deep_extend("force", json, M.kits)
-    local file = io.open(vscode_path, "w+")
-    if file then
-        file:write(vim.json.encode(json))
-        file:close()
-    end
+    utils.save_data(save_path, M.kits, true)
 end
 
 M.scan_for_kits = function()
--- lua/cmake-kits/utils.lua
+++ lua/cmake-kits/utils.lua
@@ -28,6 +28,38 @@ M.get_cmake_root = function(path)
     return vim.fs.dirname(found[#found])
 end
 
+--- @param path string
+--- @return table
+M.load_data = function(path)
+    local fd = io.open(path, "r")
+    local data = {}
+    if fd then
+        data = vim.json.decode(fd:read("*a"))
+        fd:close()
+    end
+    return data
+end
+
+--- @param path string
+--- @param data table
+--- @param update boolean modifies data already written && save new data.
+M.save_data = function(path, data, update)
+    local old_data = {}
+    if update then
+        local fd = io.open(path, "r")
+        if fd then
+            old_data = vim.json.decode(fd:read("*a"))
+            fd:close()
+        end
+    end
+    local fd = io.open(path, "w+")
+    if fd then
+        local new_data = vim.tbl_deep_extend("force", old_data, data)
+        fd:write(vim.json.encode(new_data))
+        fd:close()
+    end
+end
+
 M.notify = function(title, message, log_level)
     log_level = log_level or vim.log.levels.ERROR
     vim.notify(message, log_level, {
